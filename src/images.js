b828d4732aecd396092faec3e525deced58463f6
Parrurox <shaikhparwez963@gmail.com>
2023-07-09 12:49:22 +0530

    shop and image data

---
+++ src/images.js
@@ -0,0 +1,164 @@
+import Abzu from "./assets/game-images/Abzu - adventure.jpg";
+import AmongUs from "./assets/game-images/among us - puzzle.webp";
+import AsDuskFalls from "./assets/game-images/As Dusk Falls -adventure.jpg";
+import Bastion from "./assets/game-images/Bastion -rpg.png";
+import BattleBlockTheater from "./assets/game-images/BattleBlock Theater - platformer.png";
+import Blasphemous from "./assets/game-images/Blasphemous - action.jpg";
+import Brothers from "./assets/game-images/Brothers A Tale of Two Sons - adventure.jpg";
+import Celeste from "./assets/game-images/Celeste - platformer.png";
+import ChainedEchoes from "./assets/game-images/Chained Echoes - rpg.jpg";
+import CitizenSleeper from "./assets/game-images/Citizen Sleeper - adventure.webp";
+import Crypt from "./assets/game-images/Crypt of the Necrodancer - adventure.jpg";
+import Cuphead from "./assets/game-images/Cuphead - action.png";
+import DeadCells from "./assets/game-images/Dead Cells - action.png";
+import DeepRockGalactic from "./assets/game-images/Deep Rock Galactic - action.jpeg";
+import DiscoElysium from "./assets/game-images/Disco Elysium - RPG.jpg";
+import DokiDokiLiteratureClub from "./assets/game-images/Doki Doki Literature Club Plus - adventure.jpeg";
+import FallGuys from "./assets/game-images/Fall Guys Ultimate Knockout -action.jpg";
+import Fez from "./assets/game-images/Fez - platformer.png";
+import FireWatch from "./assets/game-images/Firewatch - adventure.jpg";
+import FTL from "./assets/game-images/FTL Faster Than Light - simulation.webp";
+import Furi from "./assets/game-images/Furi - Action.jpg";
+import GoneHome from "./assets/game-images/Gone Home - simulation.jpg";
+import Gorogoa from "./assets/game-images/Gorogoa - puzzle.jpg";
+import Gris from "./assets/game-images/Gris - puzzle.jpg";
+import Hades from "./assets/game-images/Hades - Action.jpg";
+import HerStory from "./assets/game-images/Her Story - puzzle.webp";
+import HollowKnight from "./assets/game-images/Hollow Knight - action.jpg";
+import HotlineMiami from "./assets/game-images/Hotline Miami -action.jpg";
+import Ikaruga from "./assets/game-images/Ikaruga -puzzle.webp";
+import Inscryption from "./assets/game-images/Inscryption - horror.jpg";
+import Inside from "./assets/game-images/Inside - puzzle.jpg";
+import JazzPunk from "./assets/game-images/Jazzpunk - adventure.jpg";
+import Journey from "./assets/game-images/Journey - adventure.jpg";
+import KatanaZero from "./assets/game-images/Katana Zero - platformer.png";
+import Limbo from "./assets/game-images/Limbo - puzzle.jpg";
+import LisaThePainful from "./assets/game-images/Lisa The Painful - rpg.jpg";
+import LittleNightmares from "./assets/game-images/Little Nightmares II - horror.jpg";
+import MonumentValley from "./assets/game-images/Monument Valley - platformer.webp";
+import NightInTheWoods from "./assets/game-images/Night in the Woods - adventure.webp";
+import Observation from "./assets/game-images/Observation - simulation.jpg";
+import OuterWilds from "./assets/game-images/Outer Wilds - adventure.jpg";
+import Outlast from "./assets/game-images/Outlast -horror.jpg";
+import OxenFree from "./assets/game-images/OxenFree - puzzle.jpg";
+import PapersPlease from "./assets/game-images/Papers, Please -simulation.jpg";
+import ObraDinn from "./assets/game-images/Return of the Obra Dinn- adventure.jpg";
+import RocketLeague from "./assets/game-images/Rocket League- action.webp";
+import ShovelKnight from "./assets/game-images/Shovel Knight - platformer.jpg";
+import Sifu from "./assets/game-images/Sifu -action.webp";
+import Soma from "./assets/game-images/Soma - horror.jpg";
+import Spelunky from "./assets/game-images/Spelunky 2 - platformer.jpg";
+import Spiritfarer from "./assets/game-images/Spiritfarer - simulation.jpg";
+import StardewValley from "./assets/game-images/Stardew Valley - simulation.png";
+import SteamWorldDig from "./assets/game-images/SteamWorld Dig 2 - action.jpg";
+import Subnautica from "./assets/game-images/Subnautica - adventue.png";
+import SuperHotVR from "./assets/game-images/Superhot VR - action.webp";
+import Terraria from "./assets/game-images/Terraria - adventure.jpg";
+import TheArtfulEscape from "./assets/game-images/The Artful Escape - platformer.png";
+import TheBindingOfIsaac from "./assets/game-images/The Binding of Isaac Rebirth -rpg.jpg";
+import TheCaseOfTheGoldenIdol from "./assets/game-images/The Case of the Golden Idol- puzzle.jpg";
+import TheForgottenCity from "./assets/game-images/The Forgotten City - rpg.jpg";
+import TheStanleyParable from "./assets/game-images/The Stanley Parable Ultra Deluxe -adventure.jpg";
+import ToTheMoon from "./assets/game-images/To the Moon - adventure.jpg";
+import Transistor from "./assets/game-images/Transistor -rpg.jpg";
+import Tunic from "./assets/game-images/Tunic -rpg.jpg";
+import Undertale from "./assets/game-images/Undertale -rpg.webp";
+import UntitledGooseGame from "./assets/game-images/Untitled Goose Game - simulation.webp";
+import VampireSurvivors from "./assets/game-images/Vampire Survivors - puzzle.jpg";
+import WhatRemainsOfEdithFinch from "./assets/game-images/What Remains of Edith Finch - adventure.jpg";
+
+import windows from "./assets/plat-icons/windows.png";
+import mac from "./assets/plat-icons/mac.png";
+import linux from "./assets/plat-icons/linux.png";
+
+import Action from "./assets/genre-pics/Action.png";
+import Adventure from "./assets/genre-pics/Adventure.png";
+import Horror from "./assets/genre-pics/Horror.jpeg";
+import Platformer from "./assets/genre-pics/Platformer.png";
+import Puzzle from "./assets/genre-pics/Puzzle.jpg";
+import RPGs from "./assets/genre-pics/RPGs.png";
+import Simulation from "./assets/genre-pics/Simulation.png";
+
+const images = {
+  Abzu,
+  AmongUs,
+  AsDuskFalls,
+  Bastion,
+  BattleBlockTheater,
+  Blasphemous,
+  Brothers,
+  Celeste,
+  ChainedEchoes,
+  CitizenSleeper,
+  Crypt,
+  Cuphead,
+  DeadCells,
+  DeepRockGalactic,
+  DiscoElysium,
+  DokiDokiLiteratureClub,
+  FallGuys,
+  Fez,
+  FireWatch,
+  FTL,
+  Furi,
+  GoneHome,
+  Gorogoa,
+  Gris,
+  Hades,
+  HerStory,
+  HollowKnight,
+  HotlineMiami,
+  Ikaruga,
+  Inscryption,
+  Inside,
+  JazzPunk,
+  Journey,
+  KatanaZero,
+  Limbo,
+  linux,
+  LisaThePainful,
+  LittleNightmares,
+  mac,
+  MonumentValley,
+  NightInTheWoods,
+  Observation,
+  OuterWilds,
+  Outlast,
+  OxenFree,
+  PapersPlease,
+  ObraDinn,
+  RocketLeague,
+  ShovelKnight,
+  Sifu,
+  Soma,
+  Spelunky,
+  Spiritfarer,
+  StardewValley,
+  SteamWorldDig,
+  Subnautica,
+  SuperHotVR,
+  Terraria,
+  TheArtfulEscape,
+  TheBindingOfIsaac,
+  TheCaseOfTheGoldenIdol,
+  TheForgottenCity,
+  TheStanleyParable,
+  ToTheMoon,
+  Transistor,
+  Tunic,
+  Undertale,
+  UntitledGooseGame,
+  VampireSurvivors,
+  WhatRemainsOfEdithFinch,
+  windows,
+
+  Action,
+  Adventure,
+  Horror,
+  Platformer,
+  Puzzle,
+  RPGs,
+  Simulation,
+};
+
+export default images;
